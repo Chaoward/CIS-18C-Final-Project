b9e904a30aa8a3d7bab8d6fad5f99ffcc74d470e
Howard <howieedu@gmail.com>
2020-12-07 01:54:42 -0800

    Update ContactsManager.java

--- massTextApp/src/src/ContactsManager.java
+++ massTextApp/src/src/ContactsManager.java
@@ -1,96 +1,157 @@
 package src;
 
-import java.util.HashMap;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.FileReader;
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+/***** ContactsManager **********
+ *  */
 
 public class ContactsManager {
-        private static final String FILE_PATH = "src\\src\\data\\contacts.txt";
-        private HashMap<String, Integer> contactList;
-        private static BufferedReader reader;
+    private static final String FILE_PATH = "src\\src\\data\\contacts.txt";
+    private HashMap<String, Integer> contactList;
+    private String curUsername;
+    private static BufferedReader reader;
+    private static PrintWriter writer;
 
-        ContactsManager() {
-            this.contactList = new HashMap<>();
-        }
+    ContactsManager() {
+        this.contactList = new HashMap<>();
+    }
 
 
 
-        /////// Methods ////////////////////////////////////////////////
+    /////// Methods ////////////////////////////////////////////////
 
-        //===== retrieve ==================================
-        /** reads contacts.txt and retrieves the passed in
-        * user's contact list as a single string. Splits the string
-        * into pairs of name and number and store s them together in contactList */
-        //=================================================
-        public void retrieve(String username) {
-            this.contactList = new HashMap<>();
-            String[] data = {};
+    //===== retrieve ==================================
+    /** reads contacts.txt and retrieves the passed in
+     * user's contact list as a single string. Splits the string
+     * into pairs of name and number and store s them together in contactList */
+    //=================================================
+    public void retrieve(String username) {
+        this.contactList = new HashMap<>();
+        String[] data = {};
 
-            try {
-                reader = new BufferedReader(new FileReader(FILE_PATH));
-                boolean userFound = false;
+        try {
+            reader = new BufferedReader(new FileReader(FILE_PATH));
+            boolean userFound = false;
 
-                while (!userFound && reader.ready()) {
-                    String[] curLine = reader.readLine().split(";");
-                    if (curLine[0].equals(username)) {
-                        data = curLine[1].trim().split("~");
-                        userFound = true;
-                    }
+            while (!userFound && reader.ready()) {
+                String[] curLine = reader.readLine().split(";");
+                if (curLine[0].equals(username)) {
+                    data = curLine[1].split("~");
+                    this.curUsername = curLine[0];
+                    userFound = true;
                 }
-                if (!userFound) {
-                    //write new user to file function here
-                    return;
+            }
+            if (!userFound) {
+                this.curUsername = username;
+                reader.close();
+                return;
+            }
+
+            String[] splitPair;
+            for (String pair : data) {
+                splitPair = pair.split("/");
+                this.contactList.put(splitPair[0], new Integer(splitPair[1].trim()));
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+    //===== *END* retrieve *END* ==============================
+
+
+
+    //===== displayContacts ===================================
+    /** displays each contact name and number form the current
+     * contactList in the console. */
+    //=========================================================
+    public void displayContacts() {
+        for (String name : this.contactList.keySet()) {
+            System.out.println(name + "  :  " + this.contactList.get(name));
+        }
+    }
+    //===== *END* displayContacts *END* =======================
+
+
+
+    //===== add ===============================================
+    /** adds a new contact with passed in name and numebr to
+     * contactList */
+    //=========================================================
+    public boolean add(String username, Integer number) {
+        if (this.contactList.containsKey(username)) return false;
+        this.contactList.put(username, number);
+        return this.contactList.containsKey(username);
+    }
+    //===== *END* add *END* ====================================
+
+
+
+    //===== remove ===========================================
+    /** removes the passed in username from contactList */
+    //========================================================
+    public boolean remove(String username) {
+        if (this.contactList.containsKey(username)) {
+            return this.contactList.remove(username, this.contactList.get(username));
+        }
+        return !this.contactList.containsKey(username);
+    }
+    //===== *END* remove *END* ===============================
+
+
+
+    public void save() {
+        File contactsFile = new File((FILE_PATH));
+        boolean saved = false;
+        String fileData = "";
+
+        try {
+            reader = new BufferedReader(new FileReader(contactsFile));
+            String[] lineData;
+
+            while (reader.ready()) {
+                lineData = reader.readLine().split(";");
+                if (lineData[0].equals(this.curUsername)) {
+                    fileData += listToString() + "\n";
+                    saved = true;
                 }
-
-                String[] splitPair;
-                for (String pair : data) {
-                    splitPair = pair.split("/");
-                    this.contactList.put(splitPair[0], new Integer(splitPair[1]));
+                else {
+                    fileData += reader.readLine() + "~";
                 }
-            } catch (IOException e) {
-                System.out.println(e.toString());
             }
+
+            reader.close();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+            return;
         }
-        //===== *END* retrieve *END* ==============================
 
-
-
-        //===== displayContacts ===================================
-        /** displays each contact name and number form the current
-         * contactList in the console. */
-        //=========================================================
-        public void displayContacts() {
-            for (String name : this.contactList.keySet()) {
-                System.out.println(name + "  :  " + this.contactList.get(name));
-            }
+        if (!saved) {
+            fileData += listToString();
         }
-        //===== *END* displayContacts *END* =======================
 
-
-
-        //===== add ===============================================
-        /** adds a new contact with passed in name and numebr to
-         * contactList */
-        //=========================================================
-        public boolean add(String username, Integer number) {
-            if (this.contactList.containsKey(username)) return false;
-            this.contactList.put(username, number);
-            return this.contactList.containsKey(username);
+        try {
+            writer = new PrintWriter(contactsFile);
+            writer.print(fileData);
+            writer.flush();
+        } catch (FileNotFoundException e) {
+            System.out.println(e.toString());
         }
-        //===== *END* remove *END* ================================
+    }
 
 
 
-        //===== remove ===========================================
-        /** removes the passed in username from contactList */
-        //========================================================
-        public boolean remove(String username) {
-            if (this.contactList.containsKey(username)) {
-                return this.contactList.remove(username, this.contactList.get(username));
-            }
-            return !this.contactList.containsKey(username);
+    private String listToString() {
+        String updatedLine = this.curUsername + ";";
+        for (String name : this.contactList.keySet()) {
+            updatedLine += name + "/" + this.contactList.get(name).toString() + "~";
         }
-        //===== *END* remove *END* ===============================
+        updatedLine += "n";
+        updatedLine = updatedLine.replace("~n", "");
+
+        return updatedLine;
+    }
 }
