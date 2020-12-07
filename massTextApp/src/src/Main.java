379255b0415529a5c15bee17695e42763b7ed245
Jason Jones <jasojone@gmail.com>
2020-12-06 21:17:41 -0800

    updated the UI

--- massTextApp/src/src/Main.java
+++ massTextApp/src/src/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        ContactsManager contactsManager =  new ContactsManager();
         Scanner input = new Scanner(System.in);
         ArrayList<User> users = new ArrayList();
         // Maps Username to User
@@ -41,17 +41,29 @@ public class Main {
                 System.out.println("Access Granted");
                 int userChoice = 0;
 
-                System.out.println("Hello " + username + ",\nWhat would you like to do? \n" +
+                System.out.println("Hello " + username +
+                        " What would you like to do? \n" +
                         "1. Add/Remove/Display Contacts\n" +
-                        "2. Add/Remove/Display src.Messages\n3. Send Message");
+                        "2. Add/Remove/Display Messages\n" +
+                        "3. Send Message");
                 if (userChoice == 1) {
                     System.out.println("1. Add contact\n2. Remove contact\n3. Display all contacts\n");
                     if (userChoice == 1) {
+                        Scanner myObj = new Scanner(System.in);
+                        System.out.println("Enter name:\n");
+                        String name = myObj.next();
+                        System.out.println("Enter phone number:\n");
+                        Integer number = myObj.nextInt();
+                        contactsManager.add(name, number);
 
                     } else if (userChoice == 2) {
+                        System.out.println("Enter name of contact to be removed");
+                        Scanner myObj = new Scanner(System.in);
+                        String name = myObj.next();
+                        contactsManager.remove(name);
 
                     } else if (userChoice == 3) {
-                        System.out.println("");
+                        contactsManager.displayContacts();
 
                     } else {
                         System.out.println("Invalid option");
