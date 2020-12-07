b62507cb149e9ee89aad7f71f91fc96ed30d5b96
Jason Jones <jasojone@gmail.com>
2020-12-06 22:41:09 -0800

    fixed some of the UI

--- massTextApp/src/src/Main.java
+++ massTextApp/src/src/Main.java
@@ -7,93 +7,104 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        ContactsManager contactsManager =  new ContactsManager();
+        ContactsManager contactsManager = new ContactsManager();
         Scanner input = new Scanner(System.in);
         ArrayList<User> users = new ArrayList();
         // Maps Username to User
         HashMap<String, Integer> userIndex = new HashMap();
         HashMap<String, Integer> passwordIndex = new HashMap();
         users.add(new User("user", "password"));
+        int runLoop = 1;
 
         // Create index at the end
+        do {
+            for (int i = 0; i < users.size(); i++) {
+                userIndex.put(users.get(i).getUsername(), i);
+                passwordIndex.put(users.get(i).getPassword(), i);
+            }
 
-        for (int i = 0; i < users.size(); i++) {
-            userIndex.put(users.get(i).getUsername(), i);
-            passwordIndex.put(users.get(i).getPassword(), i);
-        }
+            System.out.println("Please log in.\nEnter your username:");
+            String username = input.nextLine();
+            // if (foundUser == null) {
+            if (!(userIndex.containsKey(username))) {
+                System.out.println("Username not found");
 
-        System.out.println("Please log in.\nEnter your username:");
-        String username = input.nextLine();
-        // if (foundUser == null) {
-        if (!(userIndex.containsKey(username))) {
-            System.out.println("Username not found");
-            return;
-        } else {
-            // Check the password
-            User foundUser = users.get(userIndex.get(username));
+            } else {
+                // Check the password
+                User foundUser = users.get(userIndex.get(username));
 
-            //System.out.println(foundUser);
+                //System.out.println(foundUser);
 
-            System.out.println("Please enter your password:");
+                System.out.println("Please enter your password:");
 
-            String password = input.nextLine();
-            if (foundUser.getPassword().equals(password)) {
-                System.out.println("Access Granted");
-                int userChoice = 0;
-
-                System.out.println("Hello " + username +
-                        " What would you like to do? \n" +
-                        "1. Add/Remove/Display Contacts\n" +
-                        "2. Add/Remove/Display Messages\n" +
-                        "3. Send Message");
-                if (userChoice == 1) {
-                    System.out.println("1. Add contact\n2. Remove contact\n3. Display all contacts\n");
+                String password = input.nextLine();
+                if (foundUser.getPassword().equals(password)) {
+                    System.out.println("------Access Granted------\n" +
+                            "Hello " + username + " Welcome to the Mass Text Message Sender!");
+                    int userChoice = 0;
+                    System.out.println("What would you like to do? \n" +
+                            "1. Add/Remove/Display Contacts\n" +
+                            "2. Add/Remove/Display Messages\n" +
+                            "3. Send Message\n" +
+                            "4. Log out");
                     if (userChoice == 1) {
-                        Scanner myObj = new Scanner(System.in);
-                        System.out.println("Enter name:\n");
-                        String name = myObj.next();
-                        System.out.println("Enter phone number:\n");
-                        Integer number = myObj.nextInt();
-                        contactsManager.add(name, number);
-
-                    } else if (userChoice == 2) {
-                        System.out.println("Enter name of contact to be removed");
-                        Scanner myObj = new Scanner(System.in);
-                        String name = myObj.next();
-                        contactsManager.remove(name);
-
-                    } else if (userChoice == 3) {
-                        contactsManager.displayContacts();
-
-                    } else {
-                        System.out.println("Invalid option");
-                        return;
-                    }
-
-                } else if (userChoice == 2) {
-                    if (userChoice == 1) {
-                        System.out.println("1. Add message\n2. Remove message\n3. Display all message\n");
+                        System.out.println("1. Add contact\n2. Remove contact\n3. Display all contacts\n");
                         if (userChoice == 1) {
+                            System.out.println("Enter name:\n");
+                            String name = input.next();
+                            System.out.println("Enter phone number:\n");
+                            Integer number = input.nextInt();
+                            contactsManager.add(name, number);
 
                         } else if (userChoice == 2) {
+                            System.out.println("Enter name of contact to be removed");
+                            String name = input.next();
+                            contactsManager.remove(name);
 
                         } else if (userChoice == 3) {
+                            contactsManager.displayContacts();
+                            System.out.println("Return to main ");
+
+                        } else if (userChoice == 4) {
+                            runLoop = 1;
 
                         } else {
                             System.out.println("Invalid option");
-                            return;
+                            runLoop = 1;
                         }
 
+                    }
+                    else if (userChoice == 2) {
+                        if (userChoice == 1) {
+                            System.out.println("1. Add message\n2. Remove message\n3. Display all message\n");
+                            if (userChoice == 1) {
+                                //add a message to the messageManager
 
-                    } else if (userChoice == 3) {
+                            } else if (userChoice == 2) {
+                                // delete a message from the messageManager
+
+                            } else if (userChoice == 3) {
+                                //display all the current messages
+
+                            } else {
+                                System.out.println("Invalid option");
+                                return;
+                            }
+
+
+                        } else if (userChoice == 3) {
+
+                        }
+
+                    } else {
 
                     }
-
-                } else {
+                }else {
                     System.out.println("Access Denied");
+                    runLoop = 1;
                 }
             }
-        }
+        }while(runLoop == 1);
     }
 }
 
