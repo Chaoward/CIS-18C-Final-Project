6bdccb9591ce011af40290afc9dee50da805b6ad
Jason Jones <jasojone@gmail.com>
2020-12-06 16:09:33 -0800

    user names and passwords added

--- massTextApp/src/src/Main.java
+++ massTextApp/src/src/Main.java
@@ -1,5 +1,4 @@
 package src;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -13,13 +12,18 @@ public class Main {
         // Maps Username to User
         HashMap<String, Integer> userIndex = new HashMap();
         HashMap<String, Integer> passwordIndex = new HashMap();
+        users.add(new User("Jason","jones", 45));
+        users.add(new User("Adrian","adrval30", 36));
+        users.add(new User("Howard ","12345yesthepassword", 44));
+        users.add(new User("Carlos","garcia", 23));
         // Create index at the end
+
         for (int i = 0; i < users.size(); i++) {
             userIndex.put(users.get(i).getUsername(), i);
             passwordIndex.put(users.get(i).getPassword(), i);
         }
 
-        System.out.println("Please log in.  Enter your username:");
+        System.out.println("Please log in. Enter your username:");
         String username = input.nextLine();
         // if (foundUser == null) {
         if (!(userIndex.containsKey(username))) {
